a670ace9c25b37b21c8b23a3d5b247b68383f286
AnandAK07 <anandkumark055@gmail.com>
2024-02-04 23:24:54 +0530

    deploy

--- controllers/userController.js
+++ controllers/userController.js
@@ -31,7 +31,7 @@ const login=async(req,res)=>{
     try {
         const { email, password } = req.body
         console.log({ email, password })
-        const user = await userModel.findOne({ email });
+        const user = await userModel.find({ email: email });
         console.log(user)
         const userId = user._id.toString()
         console.log(userId)
